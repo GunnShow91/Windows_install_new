4762e0fdb2da06f5ea4cd1c21ae2c98f72535b73
GunnShow91 <53846112+GunnShow91@users.noreply.github.com>
2021-09-08 10:39:45 -0600

    Update Default_install.ps1
    added wsl feature

--- Default_install.ps1
+++ Default_install.ps1
@@ -435,7 +435,8 @@ Get-ChildItem -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\BackgroundA
 }
 
 [void]$Form.ShowDialog()
-
+#choco install
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-#choco install
+#install wsl
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
